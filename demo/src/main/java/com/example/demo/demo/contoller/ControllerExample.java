2b4fb399525149315aa8ad6e343338de7243ac5e
RANJEET KURMI <63844740+Ranjeetkurmi@users.noreply.github.com>
2026-05-25 17:01:39 +0530

    Update ControllerExample.java

--- demo/src/main/java/com/example/demo/demo/contoller/ControllerExample.java
+++ demo/src/main/java/com/example/demo/demo/contoller/ControllerExample.java
@@ -2,16 +2,16 @@ package com.example.demo.demo.contoller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerExample {
 
     @GetMapping("/welcome")
-    public String welcomePage(Model model){
-        // pass data to HTML page
-        model.addAttribute("message","Welcome to Sprinb boot!");
-        return "welcome";
+    public String welcomePage(@RequestParam String name){
+        System.out.println(name);
+        return "Rahul";
+
     }
 }
